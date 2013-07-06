9108c53a3fba06e60720f35e480c02d4cc12acbe
knuxus <knuxus@gmail.com>
2013-07-06 15:32:19 +0100

    tic tac toe login iniciado

---
+++ my_app.js
@@ -0,0 +1,37 @@
+Messages = new Meteor.Collection('messages');
+
+if (Meteor.isClient) {
+
+  Template.messages.messages = function(){
+    return Messages.find({}, { sort: { time: -1 }});
+  }
+
+
+  Template.entryfield.events = {
+    "keydown #message": function(event){
+
+      if(event.which == 13){
+      // Submit the form
+      var emails = Meteor.user().emails[0].address;
+      console.log(emails)
+      var message = document.getElementById('message');
+
+      if(message.value != ''){
+        Messages.insert({
+          emails: emails,
+          message: message.value,
+          time: Date.now()
+        });
+
+        message.value = '';
+      }
+    }
+  }
+}
+}
+
+if (Meteor.is_server) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
