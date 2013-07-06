bf48dd3af7e6899d24986af67d8d0d2b29bea65b
knuxus <knuxus@gmail.com>
2013-07-06 19:58:26 +0100

    jogadas a falhar ao passar pelo servidor

--- my_app.js
+++ my_app.js
@@ -1,75 +1,79 @@
 Messages = new Meteor.Collection('messages');
+Jogadas = new Meteor.Collection('jogadas');
+
 var player1 = true;
 var red = new Array();
 var blue = new Array();
 
 var checkwin = function(){
-alert(Arrays.asList(red).contains(0));
-  
-  if(countClick > 8){
-    alert("no one won" + countClick)
-  }
+
 };
 
 if (Meteor.isClient) {
   var countClick = 0;
 
+  Template.jogadas.jogadas = function(){
+    return Jogadas.find({}, { sort: { time: -1 }});
+  }
+
+
+
   Template.messages.messages = function(){
     return Messages.find({}, { sort: { time: -1 }});
   }
+
 
   Template.gameboard.events ={
     'click': function (event) {
 
       var clickedElement = event.target;
+
       console.log(clickedElement.id);
 
-  if(player1 == true){
-     $(clickedElement).css("background-color",'#f00');
-     player1 = false;
-     red.push(clickedElement.id)
-    }
-  else{
-    $(clickedElement).css("background-color",'#00f');
-    player1 = true;
-    blue.push(clickedElement.id)
-}
+      Jogadas.insert({
+        jogadas: clickedElement.id,
+        time: Date.now()
+      });
 
-      checkwin();
-      countClick++;
-    }
-
-  }
-
-
-  Template.entryfield.events = {
-    "keydown #message": function(event){
-
-      if(event.which == 13){
-      // Submit the form
-      var emails = Meteor.user().emails[0].address;
-
-      console.log(emails)
-
-      var message = document.getElementById('message');
-
-      if(message.value != ''){
-        Messages.insert({
-          emails: emails,
-          message: message.value,
-          time: Date.now()
-        });
-
-        message.value = '';
-      }
+      if(player1 == true){
+       $(clickedElement).css("background-color",'#f00');
+        player1 = false;
+       red.push(clickedElement.id)
+     }
+     else{
+      $(clickedElement).css("background-color",'#00f');
+      player1 = true;
+      blue.push(clickedElement.id)
     }
   }
 }
+
+Template.entryfield.events = {
+  "keydown #message": function(event){
+
+    if(event.which == 13){
+    // Submit the form
+    var emails = Meteor.user().emails[0].address;
+
+    console.log(emails)
+
+    var message = document.getElementById('message');
+
+    if(message.value != ''){
+      Messages.insert({
+        emails: emails,
+        message: message.value,
+        time: Date.now()
+      });
+
+      message.value = '';
+    }
+  }
 }
-
-if (Meteor.is_server) {
-  Meteor.startup(function () {
-
-
+}
+}
+  if (Meteor.is_server) {
+    Meteor.startup(function () {
+    //nada
   });
 }
